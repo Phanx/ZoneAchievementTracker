8a529eb4332a385edbb8f25bc0b3f55c60c1f82f
Phanx <addons@phanx.net>
2014-11-12 06:19:11 -0800

    Post SVN->Git cleanup

--- ZoneAchievementTracker.lua
+++ ZoneAchievementTracker.lua
@@ -4,13 +4,7 @@
 	Copyright (c) 2012-2014 Phanx <addons@phanx.net>. All rights reserved.
 	http://www.wowinterface.com/downloads/info20975-ZoneAchievementTracker.html
 	http://www.curse.com/addons/wow/zoneachievementtracker
-
-	Please DO NOT upload this addon to other websites, or post modified
-	versions of it. However, you are welcome to include a copy of it
-	WITHOUT CHANGES in compilations posted on Curse and/or WoWInterface.
-	You are also welcome to use any/all of its code in your own addon, as
-	long as you do not use my name or the name of this addon ANYWHERE in
-	your addon, including its name, outside of an optional attribution.
+	https://github.com/Phanx/ZoneAchievementTracker
 ----------------------------------------------------------------------]]
 
 local ENABLE_DEBUGGING = false
